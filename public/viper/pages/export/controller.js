8275b0d0fa76204c9b8c1407a10a65b6899fb4b3
Marius Wöste <mar.w@wwu.de>
2017-08-23 15:47:22 +0200

    Replace FileSaver with plain download link

--- public/viper/pages/export/controller.js
+++ public/viper/pages/export/controller.js
@@ -1,20 +1,7 @@
 var module = angular.module('de.imi.marw.viper.export', [
-  'ngFileSaver'
 ])
-.controller('ExportPageCtrl', function ($http, FileSaver) {
+.controller('ExportPageCtrl', function () {
 
   var Ctrl = this;
 
-  Ctrl.downloadAllAsCsv = downloadAllAsCsv;
-
-  function downloadAllAsCsv () {
-    $http({
-      url: '/api/variant-table/export-clustered',
-      method: 'GET',
-      responseType: 'blob'
-    })
-    .then(function (res) {
-      FileSaver.saveAs(res.data, 'viper-all.csv');
-    });
-  }
 });
