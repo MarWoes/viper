c20eb8ff5c446233a1c79fc3af6645c56efe8338
Marius Wöste <mar.w@wwu.de>
2018-01-31 15:24:40 +0100

    Add automatic zip creation to building
    That was about time...

--- build.sh
+++ build.sh
@@ -9,3 +9,14 @@ grunt
 cp -r build/grunt src/main/resources/public
 
 gradle fatJar
+
+rm -rf build/zip
+mkdir -p build/zip
+cp build/libs/VIPER-all.jar build/zip/VIPER.jar
+cp config.json build/zip/
+cp igv.properties build/zip/
+cp igv.jar build/zip/
+cp start.bat build/zip/
+
+cd build/zip
+zip viper-$(git describe --tags).zip *
